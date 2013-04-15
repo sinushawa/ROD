0261a8663fb50516a23aec7bdf9858dc2d6517c1
sinushawa <jeje_latteux@hotmail.com>
2013-04-15 17:57:09 +0200

    Animation class structure 019

--- ROD_core/Graphics/Animation/Joint.cs
+++ ROD_core/Graphics/Animation/Joint.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SharpDX;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ROD_core.Graphics.Animation
 {
-    // this model only take bones rotation into account.
-    public class Joint
+    [Serializable]
+    public class Joint : ISerializable
     {
         public int id;
+        public string name;
         public Joint parent;
         public List<Joint> children;
-
-        // this would be the original position of the joint before transformation.
-        public Vector3 position;
         public ROD_core.Mathematics.DualQuaternion localRotationTranslation;
+
+        public Joint(int _id, string _name, object _parent, ROD_core.Mathematics.DualQuaternion _localRotationTranslation)
+        {
+            id = _id;
+            name = _name;
+            if (_parent != null)
+            {
+                parent = (Joint)_parent;
+            }
+            children = new List<Joint>();
+            localRotationTranslation = _localRotationTranslation;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", id, typeof(int));
+            info.AddValue("name", name, typeof(string));
+            info.AddValue("parent", parent, typeof(Joint));
+            info.AddValue("children", children, typeof(List<Joint>));
+            info.AddValue("localRotationTranslation", localRotationTranslation, typeof(ROD_core.Mathematics.DualQuaternion));
+        }
     }
 }
---
+++ ROD_core/Graphics/Animation/Skeleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ROD_core.Graphics.Animation
+{
+    [Flags]
+    public enum SkeletonArchetype
+    {
+        None = (1 << 0),
+        Biped = (1 << 1),
+        Quadruped = (1 << 1)
+    }
+    public class Bone
+    {
+        string name;
+
+    public class Skeleton
+    {
+        public string name;
+        public SkeletonArchetype skeletonType;
+        public List<string> description;
+    }
+}
